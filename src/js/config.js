8fecf00c5b37686d50ae022053cf3506c76ca5da
Aliandr1 <marinakuznetsovova@gmail.com>
2024-11-02 15:39:58 +0100

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '09e6fb57-e387-4823-ae26-bd11b28c091a';
+export const MODAL_CLOSE_SECONDS = 2.5;
